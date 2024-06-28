580a4613f1930b7eb416b92dc232dba36ae80a85
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2024-06-28 01:42:10 -0700

    improve test coverage (#13)

--- app/role_service/models/role_crud.py
+++ app/role_service/models/role_crud.py
@@ -22,3 +22,9 @@ def create_role(session: Session, role: RoleCreate) -> PydanticRole:
 def get_all_roles(session: Session) -> list[PydanticRole]:
     roles = session.query(Role).all()
     return [PydanticRole.model_validate(role) for role in roles]
+
+
+def db_get_role_by_id(session: Session, role_id: int) -> PydanticRole:
+    role = session.query(Role).filter(Role.id == role_id).first()
+    print(role)
+    return PydanticRole.model_validate(role)
--- tests/user_service/routers/test_user_routers.py
+++ tests/user_service/routers/test_user_routers.py
@@ -14,3 +14,15 @@ def test_create_user(client: TestClient):
     
     assert len(response.json()) == 1
     assert response.json()[0]["username"] == "test_user"
+
+
+def test_get_user_by_id(client: TestClient, add_user):
+    add_user({"email": "test_user@test_domain.com", "username": "test_user", "password": "test_password"})
+
+    response = client.get(
+        url="/users/1",
+    )
+
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["email"] == "test_user@test_domain.com"
