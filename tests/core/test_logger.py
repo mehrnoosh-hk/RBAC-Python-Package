4079eb7da2a75761f32b9bb461d31aa387a3d350
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2024-06-28 13:45:01 -0700

    fix tests

--- tests/core/test_logger.py
+++ tests/core/test_logger.py
@@ -1,13 +1,14 @@
 import logging
-from app.core import logger
+from app.core.logger import logger
 
 
 def test_logger():
-    assert isinstance(logger.logger, logging.Logger)
-    assert logger.logger.level == logging.DEBUG
-    assert logger.logger.handlers[0].encoding == "utf-8"
+    assert isinstance(logger, logging.Logger)
+    logger.debug(logger.level)
+    assert logger.level == logging.DEBUG
+    assert logger.handlers[0].encoding == "utf-8"
     assert (
-        logger.logger.handlers[0].formatter._fmt
+        logger.handlers[0].formatter._fmt
         == "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
-    assert logger.logger.handlers[0].formatter.datefmt == "%m/%d/%Y %I:%M:%S %p"
+    assert logger.handlers[0].formatter.datefmt == "%m/%d/%Y %I:%M:%S %p"
